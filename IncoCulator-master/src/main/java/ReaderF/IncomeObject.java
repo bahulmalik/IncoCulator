79608907db4f95ffdac2d3eebfcdc5c0588a0a7b
Bahul.Malik <bahul.malik.bm@gmail.com>
2019-01-16 11:00:15 +0530

    New Changes

--- IncoCulator-master/src/main/java/ReaderF/IncomeObject.java
+++
@@ -1,9 +0,0 @@
-package ReaderF;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "ReaderF")
-public class IncomeObject {
-}
